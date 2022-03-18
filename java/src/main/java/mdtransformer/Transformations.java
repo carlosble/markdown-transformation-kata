624745aa19d9fdc0910fa4d2b3f65af5358809c9
carlosble <ble.jurado@gmail.com>
2022-03-18 14:04:52 +0000

    refactor: simplify and rename

--- java/src/main/java/mdtransformer/Transformations.java
+++ java/src/main/java/mdtransformer/Transformations.java
@@ -5,15 +5,11 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Transformations {
-    public List<String> linkToFootNote(String link) {
-        //"[some link](url)"
-        String linkPattern = "\\[(.+?)]\\((.+?)\\)";
-        if (link.matches(linkPattern)){
-            Pattern pattern = Pattern.compile(linkPattern);
-            Matcher matcher = pattern.matcher(link);
-            if (matcher.find()) {
-                return List.of(matcher.group(1) + " [^anchor1]", "[^anchor1]: " + matcher.group(2));
-            }
+    public List<String> linkToFootNote(String line) {
+        Pattern pattern = Pattern.compile("\\[(.+?)]\\((.+?)\\)");
+        Matcher matcher = pattern.matcher(line);
+        if (matcher.find()) {
+            return List.of(matcher.group(1) + " [^anchor1]", "[^anchor1]: " + matcher.group(2));
         }
         return List.of();
     }
