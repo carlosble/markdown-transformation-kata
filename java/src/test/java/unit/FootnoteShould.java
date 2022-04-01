80af849ac2aea3762f73629cffc4dc53da0adde1
Carlos Ble <carlos@leanmind.es>
2022-04-01 12:46:23 +0100

    refactor: extract setup and renam

--- java/src/test/java/unit/FootnoteShould.java
+++ java/src/test/java/unit/FootnoteShould.java
@@ -1,34 +1,36 @@
 package unit;
 
 import mdtransformer.Footnote;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class FootnoteShould {
 
+    private Footnote footnote;
+
+    @BeforeEach
+    public void setUp() {
+        footnote = new Footnote("some link", "url");
+    }
+
     @Test
     void generate_valid_markdown_format_for_text_in_page_and_anchor() {
-        Footnote footnote = new Footnote("some link", "url");
-
         assertThat(footnote.anchor()).matches("\\[\\^anchor_.*]: url");
         assertThat(footnote.textInPage()).matches("some link \\[\\^anchor_.*]");
     }
 
     @Test
     void use_the_same_unique_id_for_text_in_page_and_anchor() {
-        Footnote footnote = new Footnote("some link", "url");
-
         assertThat(extractIdFromFootnote(footnote.anchor()))
                 .isEqualTo(extractIdFromFootnote(footnote.textInPage()));
     }
 
     @Test
     void generate_unique_id_per_footnote() {
-        Footnote footnote = new Footnote("some link", "url");
-        Footnote footnote2 = new Footnote("some link", "url");
-
-        assertThat(footnote.anchor()).isNotEqualTo(footnote2.anchor());
+        Footnote duplicatedFootnote = new Footnote("some link", "url");
+        assertThat(this.footnote.anchor()).isNotEqualTo(duplicatedFootnote.anchor());
     }
 
     private String extractIdFromFootnote(String anchor) {
