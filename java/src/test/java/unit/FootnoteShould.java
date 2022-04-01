c422c171e98362b10090e365af92148115e18728
Carlos Ble <carlos@leanmind.es>
2022-04-01 12:41:08 +0100

    refactor: split test according to business rules

--- java/src/test/java/unit/FootnoteShould.java
+++ java/src/test/java/unit/FootnoteShould.java
@@ -6,15 +6,28 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class FootnoteShould {
+
     @Test
-    void have_a_unique_anchor_identifier() {
+    void generate_valid_markdown_format_for_text_in_page_and_anchor() {
         Footnote footnote = new Footnote("some link", "url");
-        Footnote footnote2 = new Footnote("some link", "url");
 
         assertThat(footnote.anchor()).matches("\\[\\^anchor_.*]: url");
         assertThat(footnote.textInPage()).matches("some link \\[\\^anchor_.*]");
+    }
+
+    @Test
+    void use_the_same_unique_id_for_text_in_page_and_anchor() {
+        Footnote footnote = new Footnote("some link", "url");
+
         assertThat(extractIdFromFootnote(footnote.anchor()))
                 .isEqualTo(extractIdFromFootnote(footnote.textInPage()));
+    }
+
+    @Test
+    void generate_unique_id_per_footnote() {
+        Footnote footnote = new Footnote("some link", "url");
+        Footnote footnote2 = new Footnote("some link", "url");
+
         assertThat(footnote.anchor()).isNotEqualTo(footnote2.anchor());
     }
 
